cdf6701478298230e75953dc4ab56ef27013f7e9
WebBreacher <WebBreacher@users.noreply.github.com>
2016-12-07 19:45:57 -0500

    Creating creds.py

---
+++ creds.py
@@ -0,0 +1,5 @@
+# Create a Twitter App and generate API keys then replace the XXXs below.
+consumer_key = 'XXXXXXXXXXXXXXXXXXXXXX'
+consumer_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+access_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+access_token_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
